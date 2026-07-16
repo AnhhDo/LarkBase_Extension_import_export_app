afcb0d63cfd7d1cece73f7ac8973ee8e45b54978
Vanh <anhdo.dev.lab>
2026-07-16 09:43:53 +0300

    update: getcolumn header by view

--- src/services/LarkBaseService.tsx
+++ src/services/LarkBaseService.tsx
@@ -3,10 +3,11 @@ import { bitable } from "@lark-base-open/js-sdk";
 const LarkBaseService = async () => {
   const table = await bitable.base.getActiveTable(); //active table
   const currentTableName = await table.getName(); //current table name
-  const fieldList = await table.getFieldMetaList();  //field header
-  const tableList= await bitable.base.getTableMetaList(); //table list
-  const currentView = await table.getActiveView()
+  const currentView = await table.getActiveView();
   const view = await table.getViewById(currentView.id);
+  const fieldList = await view.getFieldMetaList(); //field header
+  const tableList = await bitable.base.getTableMetaList(); //table list
+
   const fields = await table.getRecordsByPage({
     pageSize: 100,
   }); //all data record
@@ -16,7 +17,7 @@ const LarkBaseService = async () => {
     tableList: tableList,
     currentTableName: currentTableName,
     fields: fields,
-    fieldList : fieldList,
+    fieldList: fieldList,
     view: view,
     currentView: currentView,
   };
